60f9325a792ddc6006c4a744e411efac3288018e
Uss Dev 1 <ussdev1@unitesoftwaresolutions.com>
2024-12-11 14:10:06 +0530

    Nothing change

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello from the dashboard!");
-});
-
 app.use("/test", (req, res) => {
   res.send("Test from the server!");
 });
 
 app.use("/hello", (req, res) => {
-  res.send("Test from the server!");
+  res.send("Hello from the server!");
+});
+
+app.use("/", (req, res) => {
+  res.send("Hello from the dashboard!");
 });
 
 const port = 8080;
