d0b0a9639f38b95958025eb9eaf785270efbadd2
Uss Dev 1 <ussdev1@unitesoftwaresolutions.com>
2025-01-23 12:02:29 +0530

    Allow live public ip access to our backend

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://3.110.101.31"],
     credentials: true,
   })
 );
