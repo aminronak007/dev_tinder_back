f419a543bb1c5b8464a1124c25c18f22be35292c
Uss Dev 1 <ussdev1@unitesoftwaresolutions.com>
2025-01-24 18:00:10 +0530

    Build socket events

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,12 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("_"))
+    .digest("hex");
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -12,11 +20,25 @@ const initializeSocket = (server) => {
   });
 
   io.on("connection", (socket) => {
-    socket.on("joinChat", (data) => {});
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const room = getSecretRoomId(userId, targetUserId);
+      console.log(firstName, "joined Room: ", room);
+      socket.join(room);
+    });
 
-    socket.on("sendMessage", (data) => {});
+    socket.on(
+      "sendMessage",
+      ({ firstName, userId, targetUserId, text: newMessage }) => {
+        const room = getSecretRoomId(userId, targetUserId);
+        console.log(firstName, " ", newMessage);
 
-    socket.on("disconnect", () => {});
+        io.to(room).emit("getMessage", { firstName, newMessage });
+      }
+    );
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+    });
   });
 };
 
